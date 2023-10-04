c8f3fd0a69a2a19c4bce3a5ed17d3e49899c2761
Tomás del Pino Coffey <114216577+Tomasdelpinorep@users.noreply.github.com>
2023-10-04 09:25:19 +0200

    Delete APIRestCompletaLugares directory

--- APIRestCompletaLugares/src/main/java/com/salesianostriana/dam/APIRestCompletaLugares/LugarRepository.java
+++
@@ -1,6 +0,0 @@
-package com.salesianostriana.dam.APIRestCompletaLugares;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LugarRepository extends JpaRepository<Lugar,Long> {
-}
